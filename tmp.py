fd87f435bbe750173fe933039d912b9299180271
jianhuiwang <jianhuiwang@tencent.com>
2019-06-25 17:32:50 +0800

    updata

--- tmp.py
+++ tmp.py
@@ -1,6 +1,2 @@
-s = "let's it go"
-res = s.split()
-print(res)
-for item in res:
-    print(''.join(list(item)[::-1]))
-print(res)
+s = 'babad'
+print(s[::-1])
---
+++ 算法/5. 最长回文子串.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+
+def test_function():
+    solution = Solution()
+    s = 'b'
+    res = solution.longestPalindrome(s)
+    print(res)
+
+if __name__ == '__main__':
+    test_function()
